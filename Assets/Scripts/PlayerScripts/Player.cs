bffd8fa3fd614ab71d426b69b29a508c8aad803d
V. Speijer <vincentspeijer@gmail.com>
2023-03-28 22:36:51 +0200

    Added MainMenu, Added Animations, Added Battletrigger
    Mainmenu: Added a simple menu placeholder with 3 buttons, two of which work already.
    
    Animations: Added a loading screen (ISNT ACTUAL LOADING SCREEN BUT IS TO HIDE OTHER TRANSITIONS)
    Added a transition to the BattleScene
    
    Battletrigger: There is now a layer called 'Battletrigger' which tiles can be placed on. When the player interacts with these tiles, the player has a fixed chance to encounter an enemy (see player script). Still have to fetch information about the battle from the scene the player comes from (to get a fair encounter idea like pokemon).

--- Assets/Scripts/PlayerScripts/Player.cs
+++ Assets/Scripts/PlayerScripts/Player.cs
@@ -1,18 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Player : MonoBehaviour
 {
     private BoxCollider2D boxCollider;
-
-    private RaycastHit2D hit;
-
     private bool isMoving;
-
     private Vector2 origPos, targetPos;
 
     public float timeToMove = 0.15f;
+    public float battleChance = 0.1f;
+
+    public Animator transition;
+    public float transitionTime = 1f;
 
     private void Start()
     {
@@ -21,44 +22,54 @@ public class Player : MonoBehaviour
 
     private void FixedUpdate()
     {
-        if (Input.GetKey(KeyCode.W) && !isMoving)
-            StartCoroutine(MovePlayer(Vector2.up));
+        if (!isMoving)
+        {
+            if (Input.GetKey(KeyCode.UpArrow))
+                StartCoroutine(MovePlayer(Vector2.up));
 
-        if (Input.GetKey(KeyCode.A) && !isMoving)
-            StartCoroutine(MovePlayer(Vector2.left));
+            if (Input.GetKey(KeyCode.LeftArrow))
+                StartCoroutine(MovePlayer(Vector2.left));
 
-        if (Input.GetKey(KeyCode.S) && !isMoving)
-            StartCoroutine(MovePlayer(Vector2.down));
+            if (Input.GetKey(KeyCode.DownArrow))
+                StartCoroutine(MovePlayer(Vector2.down));
 
-        if (Input.GetKey(KeyCode.D) && !isMoving)
-            StartCoroutine(MovePlayer(Vector2.right));
+            if (Input.GetKey(KeyCode.RightArrow))
+                StartCoroutine(MovePlayer(Vector2.right));
+        }
     }
 
     private IEnumerator MovePlayer(Vector2 direction)
     {
         isMoving = true;
-
         float elapsedTime = 0;
         origPos = transform.position;
-
-        // Calculate the target position
         targetPos = origPos + Vector2.Scale(new Vector2((float)0.48, (float)0.48), direction);
 
         // Check if there is a blocking collider at the target position
         Collider2D hit = Physics2D.OverlapBox(targetPos, 0.5f * boxCollider.size, 0, LayerMask.GetMask("Blocking"));
-
         if (hit == null)
         {
-            while (elapsedTime < timeToMove)
+            // Check if there is a battle trigger at the target position
+            hit = Physics2D.OverlapBox(targetPos, 0.5f * boxCollider.size, 0, LayerMask.GetMask("BattleTrigger"));
+            if (hit != null && Random.value <= battleChance)
             {
-                transform.position = Vector2.Lerp(origPos, targetPos, (elapsedTime / timeToMove));
-                elapsedTime += Time.deltaTime;
-                yield return null;
+                transition.SetTrigger("StartBattle");
+
+                yield return new WaitForSeconds(transitionTime);
+
+                SceneManager.LoadScene("BattleScene");
             }
-
-            transform.position = targetPos;
+            else
+            {
+                while (elapsedTime < timeToMove)
+                {
+                    transform.position = Vector2.Lerp(origPos, targetPos, (elapsedTime / timeToMove));
+                    elapsedTime += Time.deltaTime;
+                    yield return null;
+                }
+                transform.position = targetPos;
+            }
         }
-
         isMoving = false;
     }
 }
